016118e154b91fd86c5a01f4100e0254fc39b4c2
noneno <noneno@kakao.com>
2021-03-21 23:50:20 +0900

    Planned the Movie Component

--- src/App.js
+++ src/App.js
@@ -3,48 +3,19 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 class App extends React.Component {
-  // 바꾸고 싶은 데이터를 담는 state
   state = {
-    count: 0,
+    isLoading: true,
+    // 아래처럼 미리 선언하지 않아도 없는 state를 추가하는 것엔 문제가 없다.
+    movie: [],
   };
-
-  // class 내부에서 함수를 추가하기
-  add = () => {
-    this.setState((current) => ({
-      count: current.count + 1,
-    }));
-  };
-  minus = () => {
-    this.setState((current) => ({
-      count: current.count - 1,
-    }));
-  };
-
-  // 컴포넌트를 render 하기
-  render() {
-    console.log('start rendering...!');
-    return (
-      <div>
-        <h1>The Number is ... {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-  }
-
-  // 컴포넌트 생성시 렌더링 된 후
   componentDidMount() {
-    console.log('component rendered!');
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 2000);
   }
-
-  // 컴포넌트 업데이트시 렌더링 된 후
-  componentDidUpdate() {
-    console.log('I just Updated!');
-  }
-
-  // 컴포넌트가 제거될 시
-  componentWillUnmount() {
-    console.log('byebye');
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready!'}</div>;
   }
 }
 
