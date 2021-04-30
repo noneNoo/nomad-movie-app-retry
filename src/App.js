67eeda534fc8c8165e13dc5f8c44b636264d8548
noneno <noneno@kakao.com>
2021-04-30 21:20:34 +0900

    #4.0 Fetching Movies from API (async, await)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // class 컴포넌트를 사용하기 위해서는 react를 가져와야 한다
 import React from 'react';
+import axios from 'axios';
 // import PropTypes from 'prop-types';
 
 class App extends React.Component {
@@ -8,10 +9,15 @@ class App extends React.Component {
     // 아래처럼 미리 선언하지 않아도 없는 state를 추가하는 것엔 문제가 없다.
     movie: [],
   };
+  // async 함수 내에 뭔가 기다려야 할 것이 있다고 선언
+  getMovies = async () => {
+    // await 무엇을 기다릴지 지목
+    const movies = await axios.get(
+      'https://yts-proxy.nomadcoders1.now.sh/list_movies.json'
+    );
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 2000);
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// state가 필요없기 때문에 함수형 컴포넌트 사용
+function Movie(id, year, title, summary, poster) {
+  return <h1>{title}</h1>;
+}
+
+// Movie.PropTypes = {
+//   id: PropTypes.number.isRequired,
+//   year: PropTypes.number.isRequired,
+//   title: PropTypes.string.isRequired,
+//   summary: PropTypes.string.isRequired,
+//   poster: PropTypes.string.isRequired,
+// };
+
+export default Movie;
